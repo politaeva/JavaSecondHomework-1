01ece2323d6a54eeb480583dc14ce0a930a55a00
Anna Romanovskaya <politaevaa@ya.ru>
2023-04-10 17:29:39 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        // ЗАДАНИЕ 1
+
+        int x = 13676; // Стоимость билета
+        int y = 20; // Стоимость одной мили
+        int bonus = x / y;  // Рассчет количества бонусных миль
+
+        System.out.println("Вам начислено " + bonus + " бонусных миль(и)"); // вывод на экран
+
+    }
+}
