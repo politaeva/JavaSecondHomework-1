db432edaad16fcfc8bd110b05afca54c77aa0de4
Anna Romanovskaya <politaevaa@ya.ru>
2023-04-12 18:47:53 +0300

    Доработка задания

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,10 @@ public class Main {
     public static void main(String[] args) {
         // ЗАДАНИЕ 1
 
-        int x = 13676; // Стоимость билета
-        int y = 20; // Стоимость одной мили
-        int bonus = x / y;  // Рассчет количества бонусных миль
-
-        System.out.println("Вам начислено " + bonus + " бонусных миль(и)"); // вывод на экран
+        int cost = 13676;
+        int quantity = 20;
+        int bonus = cost / quantity;
+        System.out.println(bonus + " Бонусных миль");
 
     }
 }
